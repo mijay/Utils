188cf9d992b83bf5c42ea57052b2144ca8fe40ef
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2015-10-09 18:23:46 +0200

    Add Task extensions

---
+++ Tasks/TaskExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace mijay.Utils.Tasks
+{
+#if NET40
+    public static class TaskExtensions
+    {
+        public static Task Then<T>(this Task<T> task, Action<T> action)
+        {
+            Guard.AgainstNull(task, "task");
+            Guard.AgainstNull(action, "action");
+
+            return task.ContinueWith(t => action(t.Result),
+                TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+    }
+#endif
+}
