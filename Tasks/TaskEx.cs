7c7f2ba316d7c2c138ff99812ad26706678b1d43
Mitya Kononchuk <mitya.kononchuk@mendix.com>
2015-10-13 16:57:07 +0200

    Add polyfill for `Task.FromResult`

---
+++ Tasks/TaskEx.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace mijay.Utils.Tasks
+{
+#if NET40
+    public static class TaskEx
+    {
+        public static Task<T> FromResult<T>(T value)
+        {
+#if !NET45
+            var result = new TaskCompletionSource<T>();
+            result.SetResult(value);
+            return result.Task;
+#else
+            return Task.FromResult(value);
+#endif
+        }
+    }
+#endif
+}
